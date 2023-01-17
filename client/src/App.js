98c69ede56f97d3b47ce3792797fbb1dae78e486
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-01-17 09:15:09 -0700

    Created ShoppingList component and styled with App.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar';
-function App() {
+import React, { Component } from 'react';
+import ShoppingList from './components/ShoppingList';
+
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
 <AppNavbar />
+<ShoppingList />
     </div>
   );
+}
 }
 
 export default App;
