71daa5469a02ecb1c1432a0edf11fc723d221962
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2023-01-17 11:26:28 -0700

    implementingRedux

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import AppNavbar from './components/AppNavbar';
 import React, { Component } from 'react';
 import ShoppingList from './components/ShoppingList';
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
   return (
+<Provider store={store}>
     <div className="App">
 <AppNavbar />
 <ShoppingList />
     </div>
+    </Provider>
   );
 }
 }
